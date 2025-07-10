d00fb067012c7bc39a3e126723ca2750bab17343
MohammedLouah <mohamedlwah2020@gmail.com>
2025-07-10 16:55:01 +0100

    retour 1

--- api/src/main/java/org/example/serviceplatform/DTO/AuthenticationResponse.java
+++ api/src/main/java/org/example/serviceplatform/DTO/AuthenticationResponse.java
@@ -1,11 +1,14 @@
 package org.example.serviceplatform.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.example.serviceplatform.Entities.Enums.RoleType;
-
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationResponse {
     private String accessToken;
     private String refreshToken;
